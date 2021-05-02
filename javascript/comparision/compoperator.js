c7db74a0568fd4542af549da7521e6fde3ae27aa
arjun-sudo <arjungautam5431@gmail.com>
2021-05-02 13:14:00 +0545

    varibles

---
+++ javascript/comparision/compoperator.js
@@ -0,0 +1,10 @@
+var a=10;
+
+var b="10";
+
+if(a===b){
+    console.log(a+b)
+}
+else{
+    console.log("Types are not same or not equal");
+}
---
+++ javascript/comparision/ternary.js
@@ -0,0 +1,6 @@
+var age=17;
+var result=(age<18)? "You are not eligible for voting":"You are eligible"; 
+console.log(result);
+
+console.log(typeof age)
+console.log(typeof result)
