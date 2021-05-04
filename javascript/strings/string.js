f7610fa58b5b3c868eb35d32610ac470d23b1df5
arjun-sudo <arjungautam5431@gmail.com>
2021-05-04 22:02:06 +0545

    string

---
+++ javascript/strings/string.js
@@ -0,0 +1,10 @@
+// Escape chacter 
+// \'
+// var name='It\'s me Pooja'   \"
+// var name= "It's me \"Pooja Poudel\" "
+
+// var name1 ="It's me \\Arjun"
+
+var name="PoojaPoudel";
+
+console.log(name.length)
