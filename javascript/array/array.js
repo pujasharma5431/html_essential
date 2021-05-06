7b9cf0773e9bbbc18a9e1eab9118690206309194
arjun-sudo <arjungautam5431@gmail.com>
2021-05-06 14:35:44 +0545

    array and number methods

---
+++ javascript/array/array.js
@@ -0,0 +1,16 @@
+var animals=["dog","cat","prince","arjun","pooja"]
+
+
+//loop
+
+for(let i=0;i<animals.length-1;i++){
+
+    console.log(animals[i]);
+
+}
+
+
+//forEach  -->used more js 
+
+/* To Do */
+
---
+++ javascript/numbermethods/nummethods.js
@@ -0,0 +1,4 @@
+var num=123;
+var num1=num.toString()
+
+console.log(typeof(num1))
