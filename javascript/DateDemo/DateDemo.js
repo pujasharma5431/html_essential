b08838e7d630888f755c02084f6cf5beb0d329b8
arjun-sudo <arjungautam5431@gmail.com>
2021-05-09 21:57:52 +0545

    Date() in javascript

---
+++ javascript/DateDemo/DateDemo.js
@@ -0,0 +1,15 @@
+var date=new Date();
+
+
+// console.log(date);
+// console.log(date.toDateString())
+// console.log(date.toUTCString());
+
+// console.log(date.getFullYear())
+// console.log(date.getMonth()+1);
+var day=date.getDay();
+
+var days=["sunday","monday","tuesday"];
+
+console.log(days[day]);
+
